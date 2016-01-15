2b939bdae64430d442b547366d1b2f6d0e9d1086
Jessica <a71164@alunos.uminho.pt>
2016-01-15 12:22:27 +0000

    adiciona nota ao ficheiro criacao

--- Modelos/mysql/criacao.sql
+++ Modelos/mysql/criacao.sql
@@ -4,6 +4,7 @@
 -- 2015/2016
 -- MySQL Workbench Forward Engineering
 
+-- TODO:  opçao CHECK : acrescentar restriçoes para os estados dos livros que so podem tomar certos valores 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
