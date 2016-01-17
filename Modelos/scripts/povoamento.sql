22e72376d504d0cafcbacff4311643bc6e904159
André Santos <andrerfcsantos@gmail.com>
2016-01-17 15:59:48 +0000

    Inicio povoamento

--- Modelos/scripts/povoamento.sql
+++ Modelos/scripts/povoamento.sql
@@ -14,93 +14,64 @@ USE bgum;
 SET SQL_SAFE_UPDATES = 0;
 
 INSERT INTO autor
-	(idAutor, PrimeirosNomes, Apelido)
-    VALUES
-    (1, 'Julia', 'Oliver'),
-    (2, 'Gina Bari', 'Kolata');
-
+(idAutor, PrimeirosNomes, Apelido)
+VALUES
+();
 
 INSERT INTO `autor-escreve-livro`
-	(Livro, Autor, Ano)
-    VALUES
-    (1,1,1990),
-    (2,1,1990),
-    (4,2,1990),
-    (3,2,1900);
-    
-INSERT INTO editora
-	(idEditora, Designacao)
-    VALUES
-    (1, 'River City Pub'),
-    (2, 'Black Belt Press'),
-    (3, 'Farrar Straus Giroux'),
-    (4, 'William Morrow &amp; Company');
+(Livro,Autor)
+VALUES
+();
 
-INSERT INTO livro
-	(idLivro, Titulo, CodBarras, ISBN, ISSN)
-	VALUES
-	(1,'Goodbye to the Buttermilk Sky', 22, 1881320189, 12),
-    (2,'Seventeen Times As High As the Moon: Short Stories', 11, 1881320049, 11),
-    (3, 'Clone: The Road to Dolly, and the Path Ahead', 231, 688156924, 231),
-    (4, 'Flu: The Story of the Great Influenza Pandemic of 1918 and the Search for the Virus ', 222, 374157065, 222);
-
-DELETE FROM livro;
-
-INSERT INTO `livro-publicado-editora`
-	(Livro, Editora, Edicao, Ano)
-    VALUES
-    (1,1,0,1994),
-    (2,2,0,1993),
-    (4,3,0,1999),
-    (3,4,0,1998);
-
-    
 INSERT INTO cdu
-	(CDU, Exemplar)
-    VALUES
-    (),
-    ();
-    
--- TODO ACRESCENTAR AO DICIONARIO DE DADOS E VER O QUE METER AQUI
-INSERT INTO colecao
-    ()
-    VALUES
-    (),
-    ();
+(CDU, Livro)
+VALUES
+();
+
+INSERT INTO coleccao
+(idColeccao, Designacao)
+VALUES
+();
+
+INSERT INTO editora
+(idEditora, Designacao)
+VALUES
+();
 
 INSERT INTO exemplar
-	(idExemplar, Condicao, Disponibilidade, Localizacao, Livro)
-	VALUES
-	(1,'Novo em folha', 2, 1, 1),
-    (2, 'Usado', 3, 2, 1);
-
-   
-DELETE from exemplar;
+(idExemplar, Condicao, Disponibilidade, Localizacao, Livro)
+VALUES
+();
 
 INSERT INTO `exemplar-reservado-utilizador`
-	(Exemplar, Utilizador, DataReserva, Estado)
-    VALUES
-    (),
-    ();
- 
+(Exemplar, Utilizador, DataReserva, Estado)
+VALUES
+();
 
- INSERT INTO localizacao
-	(IdLocal, Piso, Estante, Prateleira)
-    VALUES
-    (1,1,1,1),
-    (2,1,2,2);
- 
- INSERT INTO requisicao
-	(idRequisicao, DataRequisicao, DataEntrega, Estado, NroMaxRenovacoes, Exemplar)
-    VALUES
-    (),
-    ();
+INSERT INTO livro
+(idLivro, Titulo, CodBarras, ISBN, ISSN, Coleccao)
+VALUES
+();
+
+INSERT INTO `livro-publicado-editora`
+(Livro, Editora, Edicao, Ano)
+VALUES
+();
+
+INSERT INTO localizacao
+(idLocal, Piso, Estante, Prateleira)
+VALUES
+();
+
+INSERT INTO requisicao
+(idRequisicao, DataRequisicao, DataEntrega, Estado, NroMaxRenovacoes, NrRenovacoes, Exemplar, Utilizador)
+VALUES
+();
 
 INSERT INTO utilizador
-	(idUser, Tipo, Nome, Email, CC, NroMecanografico, Contacto)
-    VALUES
-    (),
-    ();
+(idUser, Tipo, Nome, Email, CC, NroMecanografico, Telefone)
+VALUES
+();
 
 
 
