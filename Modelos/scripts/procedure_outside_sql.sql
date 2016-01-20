8dbc5f4b547dac15e940750f32736870ec7c085d
Jessica <a71164@alunos.uminho.pt>
2016-01-20 01:31:39 +0000

    Preparação para triggers e procedures - templates
    altera e adiciona templates
    altera nome de povoamento.sql para povoamento_template.sql

---
+++ Modelos/scripts/procedure_outside_sql.sql
@@ -0,0 +1,10 @@
+CREATE PROCEDURE `nome_procedure` (IN input_var INT)
+BEGIN
+
+INSERT INTO Livro
+(idLivro, Titulo, CodBarras, ISBN, ISSN, Coleccao)
+    
+   -- codigo sql
+	(444,'novo',23132,3123213,3123123,1);
+        
+END
