278bb13387b6d85bd2ca018c466b61c9eab6d546
André Santos <andrerfcsantos@gmail.com>
2016-01-20 11:03:27 +0000

    Procedimentos e triggers p/ reserva e requisicao

---
+++ Modelos/scripts/procedimentos.sql
@@ -0,0 +1,156 @@
+USE bgum;
+
+delimiter \\
+CREATE PROCEDURE sp_efectuar_requisicao(IN p_IdExemplar INT, IN p_Utilizador INT, IN p_DataRequisicao DATE,  IN p_DataEntrega DATE, IN p_NrMaxRenovacoes INT)
+BEGIN
+
+DECLARE v_utilizador INT;
+DECLARE v_numeroReservas INT DEFAULT 0;
+DECLARE v_disponibilidadeExemplar INT;
+DECLARE Erro BOOL DEFAULT 0;
+DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET Erro = 1;
+
+SET autocommit = 0;
+SET SQL_SAFE_UPDATES = 0;
+START TRANSACTION;
+
+SELECT E.Disponibilidade INTO v_disponibilidadeExemplar
+	FROM Exemplar E
+    WHERE E.idExemplar = p_IdExemplar;
+    
+IF Erro OR v_disponibilidadeExemplar != 2 THEN
+	ROLLBACK;
+END IF;
+
+SELECT COUNT(Exemplar) INTO v_numeroReservas
+	FROM `exemplar-reservado-utilizador` ERU
+    WHERE Exemplar = p_IdExemplar AND Utilizador = p_Utilizador AND Estado = 1;
+
+IF v_numeroReservas > 0 OR Erro 
+	THEN
+		SELECT ERU.Utilizador INTO v_utilizador
+			FROM `exemplar-reservado-utilizador` ERU
+			WHERE Exemplar = p_IdExemplar AND Estado = 1
+			ORDER BY DataReserva ASC
+			LIMIT 1;
+	ELSE
+		ROLLBACK;
+END IF;
+
+INSERT INTO requisicao
+(DataRequisicao, DataEntrega, Estado, NroMaxRenovacoes, NrRenovacoes, Exemplar, Utilizador)
+VALUES
+(p_DataRequisicao, p_DataEntrega, 0, p_NrMaxRenovacoes, 0, p_IdExemplar, p_Utilizador);
+
+IF Erro THEN ROLLBACK; END IF;
+
+UPDATE exemplar
+	SET Disponibilidade = 1
+    WHERE idExemplar = p_IdExemplar;
+
+IF Erro THEN ROLLBACK; ELSE COMMIT; END IF;
+
+SET SQL_SAFE_UPDATES = 1;
+
+END;\\
+delimiter ;
+
+delimiter \\
+CREATE PROCEDURE sp_entregar_exemplar_requisicao(IN p_idExemplar INT)
+BEGIN
+
+DECLARE v_estadoExemplar INT;
+DECLARE v_idRequisicao INT;
+DECLARE Erro BOOL DEFAULT 0;
+DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET Erro = 1;
+
+SET autocommit = 0;
+SET SQL_SAFE_UPDATES = 0;
+
+START TRANSACTION;
+
+SELECT E.Estado INTO v_estadoExemplar
+	FROM exemplar E
+    WHERE idExemplar = p_idExemplar;
+    
+IF v_estadoExemplar != 1 OR Erro THEN ROLLBACK; END IF;
+
+UPDATE exemplar
+	SET Disponibilidade = 2
+	WHERE idExemplar = p_idExemplar;
+    
+IF Erro THEN ROLLBACK; END IF;
+
+SELECT idRequisicao INTO v_idRequisicao
+	FROM Exemplar E, Requisicao R
+    WHERE R.Estado = 0 AND E.idExemplar = R.Exemplar;
+
+IF Erro THEN ROLLBACK; END IF;
+
+UPDATE requisicao
+SET Estado = 1
+WHERE idRequisicao = v_idRequisicao;
+
+IF Erro THEN ROLLBACK; ELSE COMMIT; END IF;
+SET SQL_SAFE_UPDATES = 1;
+END;\\
+delimiter ;
+
+delimiter \\
+CREATE PROCEDURE sp_efectuar_reserva(IN p_idExemplar INT, IN p_idUtilizador INT, IN p_DataReserva DATE)
+BEGIN
+
+DECLARE v_disponibilidade INT;
+DECLARE v_estadoAInserir INT;
+DECLARE Erro BOOL DEFAULT 0;
+DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET Erro = 1;
+
+SET autocommit = 0;
+SET SQL_SAFE_UPDATES = 0;
+
+START TRANSACTION;
+
+SELECT E.Disponibilidade INTO v_disponibilidade
+	FROM exemplar E
+    WHERE E.idExemplar = p_idExemplar;
+
+IF Erro OR v_disponibilidade=0 THEN ROLLBACK; END IF;
+
+IF v_disponibilidade = 1 THEN SET v_estadoAInserir = 0; END IF;
+IF v_disponibilidade = 2 THEN SET v_estadoAInserir = 1; END IF;
+
+IF Erro THEN ROLLBACK; END IF;
+
+INSERT INTO `exemplar-reservado-utilizador`
+(Exemplar,Utilizador, DataReserva, Estado)
+VALUES
+(p_idExemplar, p_idUtilizador, p_DataReserva, v_estadoAInserir);
+
+IF Erro THEN ROLLBACK; ELSE COMMIT; END IF;
+SET SQL_SAFE_UPDATES = 1;
+END;\\
+delimiter ;
+
+delimiter \\
+CREATE PROCEDURE sp_cancelar_reserva(IN p_idExemplar INT, IN p_idUtilizador INT, IN p_DataReserva DATE)
+BEGIN
+
+DECLARE v_disponibilidade INT;
+DECLARE v_estadoAInserir INT;
+DECLARE Erro BOOL DEFAULT 0;
+DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET Erro = 1;
+
+SET autocommit = 0;
+SET SQL_SAFE_UPDATES = 0;
+START TRANSACTION;
+
+UPDATE `exemplar-reservado-utilizador`
+	SET Estado = 3
+    WHERE Exemplar = p_idExemplar AND Utilizador = p_idUtilizador AND DataReserva = p_DataReserva;
+
+IF Erro THEN ROLLBACK; ELSE COMMIT; END IF;
+SET SQL_SAFE_UPDATES = 1;
+END;\\
+delimiter ;
+
+
---
+++ Modelos/scripts/triggers.sql
@@ -0,0 +1,44 @@
+USE bgum;
+
+delimiter \\
+CREATE TRIGGER actualiza_reservas AFTER UPDATE ON exemplar
+FOR EACH ROW
+BEGIN
+
+DECLARE Erro BOOL DEFAULT 0;
+DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET Erro = 1;
+
+START TRANSACTION;
+
+SET SQL_SAFE_UPDATES = 0;
+-- Livro livre (2) passa a requisitado (1)
+IF OLD.Disponibilidade = 2 AND NEW.Disponibilidade = 1 
+	THEN
+    -- Ver todos os livros prontos a levantar (1) e mudar estado
+    -- para reserva pedida (0)
+		UPDATE `exemplar-reservado-utilizador`
+			SET Estado = 0
+            WHERE Exemplar = NEW.idExemplar AND Estado = 1
+            ORDER BY DataReserva ASC
+            LIMIT 1;
+END IF;
+
+-- Livro requisitado (1) passa a livre (2)
+IF OLD.Disponibilidade = 1 AND NEW.Disponibilidade = 2
+	THEN
+    -- Ver todos as reservas com reserva pedida (0) e mudar estado
+    -- para livro pronto a levantar
+		UPDATE `exemplar-reservado-utilizador`
+			SET Estado = 1
+            WHERE Exemplar = NEW.idExemplar AND Estado = 0
+            ORDER BY DataReserva ASC
+            LIMIT 1;
+END IF;
+
+SET SQL_SAFE_UPDATES = 1;
+
+IF Erro THEN ROLLBACK; ELSE COMMIT; END IF;
+
+END;\\
+delimiter ;
+
