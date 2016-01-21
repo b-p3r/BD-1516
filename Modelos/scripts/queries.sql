c1c9f9e2709c8889e2344ecf3cdf0bbfefeba790
André Santos <andrerfcsantos@gmail.com>
2016-01-21 18:52:00 +0000

    Modelo fisico: Transacoes e espaço

--- Modelos/scripts/queries.sql
+++ Modelos/scripts/queries.sql
@@ -4,6 +4,6 @@ SELECT table_schema 'DB Name', SUM(data_length + index_length) / 1024 / 1024 'DB
 	FROM information_schema.tables
 	WHERE table_schema = 'bgum';
 
-SELECT TABLE_NAME, table_rows, data_length, index_length, 
-round(((data_length + index_length) / 1024 / 1024),2) 'Size in MB'
+SELECT TABLE_NAME, table_rows, data_length / 1024 'Dados (KB)', index_length / 1024 'Indices (KB)', 
+round(((data_length + index_length) / 1024 / 1024),5) 'Size in MB'
 	FROM information_schema.TABLES WHERE table_schema = 'bgum';
